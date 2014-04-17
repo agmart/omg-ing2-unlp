8e9d8f69c45296a278ad6f4e18fb0a647104c8f6
Martín Aguilar <maguilar@cespi.unlp.edu.ar>
2014-04-17 01:48:05 -0300

    Inicio de rails

---
+++ cookbooks/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cookbooks::Application.config.secret_key_base = '28f7db91e3aac64f7a95a17ac8b31ab6deb5ea9fc0767eb8a54f9b4a07bd383fd04886db5302807e10774917a17805f997dffb326225918c211a72eea20c1f5a'
---
+++ cookbooks/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cookbooks::Application.config.session_store :cookie_store, key: '_cookbooks_session'
