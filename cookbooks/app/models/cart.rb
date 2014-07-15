f5d322cad1a98d0f4094fb16d613861b4a61b132
Martín Aguilar <martinaguilar3190@gmail.com>
2014-07-15 19:24:31 -0300

    Agregada posibilidad de elegir cantidad al meter un libro al carro

--- cookbooks/app/models/cart.rb
+++ cookbooks/app/models/cart.rb
@@ -3,12 +3,12 @@ class Cart < ActiveRecord::Base
   belongs_to :user
   has_many :cart_books, dependent: :destroy
   
-  def add_product(book_id)
+  def add_product(book_id, cantidad)
     current_item = cart_books.find_by_book_id(book_id)
     if current_item
-      current_item.quantity += 1
+      current_item.quantity += cantidad
     else
-      current_item = cart_books.build(book: book_id)
+      current_item = cart_books.build(book: book_id, quantity: cantidad)
     end
     current_item
   end
